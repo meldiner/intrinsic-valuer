eadd56677067c8182ceb7cc9bc5ee7fa38235429
Ron Meldiner <meldiner@dropbox.com>
2018-08-07 22:07:20 -0700

    add hacky margin of safety calculation

--- xlsx_writer.py
+++ xlsx_writer.py
@@ -9,7 +9,7 @@ def write(db, file):
     row += 1
     row = write_owner_earnings(worksheet, row, db)
     row += 1
-    row = write_sticker_price(worksheet, row, db)
+    row = write_margin_of_safety(worksheet, row, db)
 
 def write_row(worksheet, row, values):
     col = 0
@@ -17,11 +17,14 @@ def write_row(worksheet, row, values):
         worksheet.write(row, col, value)
         col += 1
 
+def get_cell(row, col):
+    return chr(64 + col) + str(row)
+
 def growth_fx(row, col):
-    return "=IFERROR(" + chr(64 + col) + str(row + 1) + "/" + chr(64 + col) + str(row) + "-1" + ", \"\")"
+    return "=IFERROR(" + get_cell(row + 1, col) + "/" + get_cell(row, col) + "-1" + ", \"\")"
 
 def average_fx(start, end, col):
-    return "=average(" + chr(64 + col) + str(start) + ":" + chr(64 + col) + str(end) + ")"
+    return "=average(" + get_cell(start, col) + ":" + get_cell(end, col) + ")"
 
 def write_big_four_numbers(worksheet, row, db):
     write_row(worksheet, row, ["Big Four Numbers"])
@@ -77,7 +80,7 @@ def write_big_four_numbers(worksheet, row, db):
     write_row(worksheet, row, values)
     row += 1
 
-    values = ["Baseline Windage Growth Rate", "=average(" + chr(64 + 2) + str(row) + ":" + chr(64 + col) + str(row) + ")"]
+    values = ["Baseline Windage Growth Rate", "=average(" + get_cell(row, 2) + ":" + get_cell(row, col) + ")"]
     write_row(worksheet, row, values)
     row += 1
 
@@ -103,18 +106,50 @@ def write_owner_earnings(worksheet, row, db):
 
     return row
 
-def write_sticker_price(worksheet, row, db):
-    write_row(worksheet, row, ["Sticker Price"])
+def write_margin_of_safety(worksheet, row, db):
+    write_row(worksheet, row, ["Margin of Safety"])
     row += 1
+
+    year = max(db["eps"].iterkeys())
+    write_row(worksheet, row, ["EPS (diluted) for " + str(year), db["eps"][year]])
+    row += 1
+    eps_row = row
+    eps_col = 2
+
+    write_row(worksheet, row, ["Windage Growth Rate", "=0.16"])
+    row += 1
+    wgr_row = row
+    wgr_col = 2
+
+    write_row(worksheet, row, ["Highest P/E", "=22"])
+    row += 1
+    hpe_row = row
+    hpe_col = 2
+
+    write_row(worksheet, row, ["Wideage P/E", "=min(" + get_cell(wgr_row, wgr_col) + "*200," + get_cell(hpe_row, hpe_col) + ")"])
+    row += 1
+    wpe_row = row
+    wpe_col = 2
 
     write_row(worksheet, row, ["Minimum Acceptable Rate of Return", "15%"])
     row += 1
 
-    write_row(worksheet, row, ["Year", "Earnings per Share (diluted)"])
+    write_row(worksheet, row, ["Future 10-Year EPS", "=" + get_cell(eps_row, eps_col) + "*(1+" + get_cell(wgr_row, wgr_col) + ")^10"])
     row += 1
+    feps_row = row
+    feps_col = 2
+    
+    write_row(worksheet, row, ["Future 10-Year Share Price", "=" + get_cell(feps_row, feps_col) + "*" + get_cell(wpe_row, wpe_col)])
+    row += 1
+    fsp_row = row
+    fsp_col = 2
 
-    year = max(db["eps"].iterkeys())
-    write_row(worksheet, row, [year, db["eps"][year]])
+    write_row(worksheet, row, ["Sticker Price", "=" + get_cell(fsp_row, fsp_col) + "/4"])
+    row += 1
+    sp_row = row
+    sp_col = 2
+
+    write_row(worksheet, row, ["Buy Price", "=" + get_cell(sp_row, sp_col) + "/2"])
     row += 1
 
     return row
